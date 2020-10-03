fde8030f402ebaf1102752267c9e569a9eb40540
a4illusionist <sohail.sohailishaq@gmail.com>
2020-10-03 11:03:58 +0500

    msg changed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to React App. This is Assignment Bootcamp 2020.3
+          Welcome to React App. This is Assignment Bootcamp 2020.
         </p>
         <a
           className="App-link"
